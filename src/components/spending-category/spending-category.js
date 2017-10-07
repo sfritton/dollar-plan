da045c25f7a25eccbb92921f219a74f696abeb77
Sam Fritton <sfritton94@gmail.com>
2017-10-07 16:04:20 -0500

    improved messages and added tests

--- src/components/spending-category/spending-category.js
+++ src/components/spending-category/spending-category.js
@@ -7,21 +7,32 @@ import ProgressBar from '../progress-bar/progress-bar';
 'use strict';
 
 class SpendingCategory extends React.Component {
+  generateMessage() {
+    if (this.props.actualAmount < this.props.plannedAmount) {
+      return `$${this.props.plannedAmount - this.props.actualAmount} left to spend`;
+    }
+
+    if (this.props.actualAmount == this.props.plannedAmount) {
+      return 'nothing left to spend';
+    }
+
+    return `$${this.props.actualAmount - this.props.plannedAmount} overbudget!`;
+  }
   render() {
     return (
       <Grid className="category">
         <Row>
-          <Col className="height-100" xs={3}>
+          <Col className="height-100" xs={3} md={2}>
             <div className="category-title">{this.props.title}</div>
           </Col>
-          <Col className="height-100" xs={1}>
+          <Col className="height-100" xs={1} md={1}>
             <div className="category-amount">{'$' + this.props.plannedAmount}</div>
           </Col>
-          <Col className="height-100" xs={4}>
+          <Col className="height-100" xs={4} md={6}>
             <ProgressBar percent={this.props.actualAmount/this.props.plannedAmount}/>
           </Col>
-          <Col className="height-100" xs={4}>
-            <div className="category-message">{'$' + this.props.actualAmount + ' spent so far'}</div>
+          <Col className="height-100" xs={4} md={3}>
+            <div className="category-message">{this.generateMessage()}</div>
           </Col>
         </Row>
       </Grid>
