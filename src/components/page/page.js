3d40def111fdadcec8d2e2c76fcad5b300749f70
Sam Fritton <sfritton94@gmail.com>
2017-10-17 20:19:50 -0500

    improving page scrolling

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -3,9 +3,25 @@ import React from "react";
 ("use strict");
 
 class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: "0", height: "0" };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
   render() {
     return (
-      <div>
+      <div style={{ height: this.state.height }}>
         <div className="pg-header">{this.props.header}</div>
         <div className="pg-body">{this.props.children}</div>
         {this.props.footer ? (
