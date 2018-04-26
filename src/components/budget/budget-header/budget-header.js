1f964f7a580207f04ee945d2bfacf11437a05906
Sam Fritton <sfritton94@gmail.com>
2018-04-25 21:29:35 -0500

    first work on budget-header, more styling

--- src/components/budget/budget-header/budget-header.js
+++ src/components/budget/budget-header/budget-header.js
@@ -1,53 +1,49 @@
 import React from "react";
-import { Grid, Row, Col } from "react-bootstrap";
+import "./budget-header.less";
 
 import HeaderDropdown from "../../util/header-dropdown";
 import DateService from "../../../services/date-service";
 
-export default class BudgetHeader extends React.Component {
-  render() {
-    return (
-      <div className="nav">
-        <HeaderDropdown
-          selected={this.mapDateToOption(this.props.date)}
-          options={this.props.budgetDates.map(date =>
-            this.mapDateToOption(date)
-          )}
-          createNewBudget={() => this.props.createNewBudget()}
-          setActiveBudget={(month, year) =>
-            this.props.setActiveBudget(month, year)}
-        />
-        <div className="days-left">{this.renderDaysLeft()}</div>
-      </div>
-    );
+const getDaysLeftMessage = date => {
+  if (!date) return "";
+
+  const { month, year } = date;
+
+  if (!DateService.hasMonthStarted(month, year)) {
+    return "Month has not started";
   }
 
-  renderDaysLeft() {
-    if (!this.props.date) {
-      return "";
-    }
-
-    const { month, year } = this.props.date;
-
-    if (!DateService.hasMonthStarted(month, year)) {
-      return "Month has not started";
-    }
-
-    if (DateService.hasMonthEnded(month, year)) {
-      return "Month has ended";
-    }
-
-    return `${DateService.getDaysLeft(month, year)} days left`;
+  if (DateService.hasMonthEnded(month, year)) {
+    return "Month has ended";
   }
 
-  mapDateToOption(date) {
-    if (!date) {
-      return null;
-    }
+  return `${DateService.getDaysLeft(month, year)} days left`;
+};
 
-    return {
-      ...date,
-      name: `${DateService.getMonthName(date.month)} ${date.year}`
-    };
-  }
-}
+const mapDateToOption = date => {
+  if (!date) return null;
+
+  return {
+    ...date,
+    name: `${DateService.getMonthName(date.month)} ${date.year}`
+  };
+};
+
+const BudgetHeader = ({
+  date,
+  budgetDates,
+  createNewBudget,
+  setActiveBudget
+}) => (
+  <div>
+    <HeaderDropdown
+      selected={mapDateToOption(date)}
+      options={budgetDates.map(date => mapDateToOption(date))}
+      createNewBudget={() => createNewBudget()}
+      setActiveBudget={(month, year) => setActiveBudget(month, year)}
+    />
+    <h2 className="nav-message">{getDaysLeftMessage(date)}</h2>
+  </div>
+);
+
+export default BudgetHeader;
