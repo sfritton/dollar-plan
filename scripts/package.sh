c3716fbab0d39fa72620cd2bf10c07da9cdaf1da
Sam Fritton <sfritton94@gmail.com>
2018-04-27 19:21:36 -0500

    package script preserves data

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,14 +3,22 @@ echo "Building dollar-plan ..."
 npm run build
 echo ""
 
+echo "Backing up data files ..."
+mkdir tmp
+cp -R app/data/ tmp/data/
+
 echo "Clearing app directory ..."
 rm -rf app/
 mkdir app
-echo ""
 
 echo "Copying files to app directory ..."
 cp -R dist/ app/dist/
 cp electron.prod.js app/main.js
 cp package.json app/package.json
+cp -R tmp/data/ app/data/
+
+echo "Deleting temporary files ..."
+rm -rf tmp/
 echo ""
+
 echo "Done!"
