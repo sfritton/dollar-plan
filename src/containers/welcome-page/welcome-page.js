130bc1f786da7d7e947f2b41726093001c62a6f6
Sam Fritton <sfritton94@gmail.com>
2019-05-08 17:49:05 -0500

    adding budget picking

--- src/containers/welcome-page/welcome-page.js
+++ src/containers/welcome-page/welcome-page.js
@@ -2,33 +2,86 @@ import React, { Component, Fragment } from "react";
 import "./welcome-page.less";
 import { connect } from "react-redux";
 
-import { createNewBudget, getBudget } from "Redux/budgets/actions";
-import { setBudgetPage, setEditing } from "Redux/ui/actions";
-import { Button, Dropdown, Page } from "Components";
+import {
+  setNewBudgetPage as setNewBudgetPageAction,
+  setBudgetPage
+} from "Redux/ui/actions";
+import { getBudget } from "Redux/budgets/actions";
+import { Button, Input } from "Components";
 import { decodeDate, getMonthName, months } from "Util/date";
+
+const matchesSearchTerm = ({ name }, searchTerm) => {
+  if (searchTerm === "") return true;
+
+  const searchRegex = new RegExp(searchTerm);
+
+  return searchRegex.test(name.toLowerCase());
+};
 
 class WelcomePage extends Component {
   constructor() {
     super();
 
-    this.state = { selectedDate: { value: 0 } };
+    this.state = { isChoosingBudget: false, searchTerm: "" };
   }
   render() {
-    const { budgetDates } = this.props;
-    const { selectedDate } = this.state;
+    const { budgetDates, setNewBudgetPage, selectBudget } = this.props;
+    const { isChoosingBudget, searchTerm } = this.state;
     const hasBudgets = budgetDates.length > 0;
+
     return (
       <div className="welcome">
-        <h1>Dollar Plan</h1>
-        {hasBudgets && (
+        <h1 className="welcome--title">Dollar Plan</h1>
+        {isChoosingBudget ? (
           <Fragment>
-            <Button className="welcome--wide-btn">Choose a budget</Button>
-            <div>or</div>
+            <h2>Choose a budget</h2>
+            <Input
+              className="welcome--typeahead"
+              value={searchTerm}
+              onChange={e => this.setState({ searchTerm: e.target.value })}
+              placeholder="Filter"
+            />
+            <div className="welcome--budgets-container">
+              {budgetDates.reduce((acc, { id, name, month, year }) => {
+                const matches = matchesSearchTerm({ name }, searchTerm);
+                if (!matches) return acc;
+
+                return [
+                  ...acc,
+                  <Button
+                    key={id}
+                    outlined
+                    className="welcome--budget-btn"
+                    onClick={() => selectBudget(month, year)}
+                  >
+                    {name}
+                  </Button>
+                ];
+              }, [])}
+            </div>
           </Fragment>
+        ) : (
+          <div className="welcome--fade-in">
+            {hasBudgets && (
+              <Fragment>
+                <Button
+                  className="welcome--wide-btn"
+                  onClick={() => this.setState({ isChoosingBudget: true })}
+                >
+                  Choose a budget
+                </Button>
+                <div>or</div>
+              </Fragment>
+            )}
+            <Button
+              className="welcome--wide-btn"
+              outlined={hasBudgets}
+              onClick={setNewBudgetPage}
+            >
+              Create a new {hasBudgets ? "one" : "budget"}
+            </Button>
+          </div>
         )}
-        <Button className="welcome--wide-btn" outlined={hasBudgets}>
-          Create a new {hasBudgets ? "one" : "budget"}
-        </Button>
       </div>
     );
   }
@@ -36,13 +89,23 @@ class WelcomePage extends Component {
 
 const mapStateToProps = state => ({
   budgetDates: Object.keys(state.budgets).map(encodedDate => {
-    const date = decodeDate(encodedDate);
+    const { month, year } = decodeDate(encodedDate);
 
     return {
-      value: encodedDate,
-      name: `${getMonthName(date.month)} ${date.year}`
+      id: encodedDate,
+      name: `${getMonthName(month)} ${year}`,
+      month,
+      year
     };
   })
 });
 
-export default connect(mapStateToProps)(WelcomePage);
+const mapDispatchToProps = dispatch => ({
+  setNewBudgetPage: () => dispatch(setNewBudgetPageAction()),
+  selectBudget: (month, year) => {
+    dispatch(getBudget(month, year));
+    dispatch(setBudgetPage());
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(WelcomePage);
